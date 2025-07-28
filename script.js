a3f17b5833af8a79846a42458b03d44c766f530c
tomlochaide0 <tomlochaider1@gmail.com>
2025-07-28 20:20:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// Código JS personalizado para o site Brendy's Buffet Livre
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Brendy's site carregado com sucesso!");
+});
